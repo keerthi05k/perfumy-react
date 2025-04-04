c18defd88801c1057620341e1daab091fd54fef9
keerthanakannan-7 <keerthanakannan3833@gmail.com>
2025-04-04 10:45:40 +0530

    update all files

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import Navbar from './component/navbar'
+import Search from './component/search'
+import Product from './component/product'
+import About from './component/about'
+import Footer from './component/footer'
+
+
+function App()
+{
+    return(<div>
+        <Navbar></Navbar>
+        <Search></Search>
+        <Product></Product>
+        <About></About>
+        <Footer></Footer>
+        </div>
+    )
+}
+
+export default App
